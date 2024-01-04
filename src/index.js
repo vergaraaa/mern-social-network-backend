f3df04b8e6f533b5d0e660ed9b63b07ec96e8388
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-04 13:18:18 +0100

    feat: initial index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
+const cors = require('cors');
+const express = require('express');
 const dbConnection = require("./database/connection");
+const morgan = require('morgan');
 
 // db connection
 dbConnection();
 
 // create node server
-
+const port = 4000 || process.env.PORT;
+const app = express();
 
 // setup cors
-
+app.use(cors());
 
 // middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
+// test route
+app.get("/test", (req, res) => {
+    return res.status(200).json({
+        name: "Edgar Vergara"
+    });
+});
 
 // load routes
 
 
 // run server
-
+app.listen(port, () => {
+    console.log("listening on port " + port);
+});
