98fd8093e35348a6adbf8568ab695d8672a5c1ff
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-08 19:15:51 +0100

    feat: get a users post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,5 +1,5 @@
 const Post = require("../models/Post");
-
+const mongoosePagination = require('mongoose-pagination');
 const createPost = async (req, res) => {
     try {
         const body = req.body;
@@ -57,7 +57,39 @@ const deletePost = async (req, res) => {
     }
 }
 
+const getUserPosts = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        let page = req.params.page ? parseInt(req.params.page) : 1
+
+        const itemsPerPage = 5;
+
+        const total = await Post.countDocuments({ user: userId });
+
+        const posts = await Post
+            .find({ user: userId })
+            .sort("-created_at")
+            .populate("user", "-password -role -__v")
+            .paginate(page, itemsPerPage);
+
+        return res.status(200).json({
+            status: "success",
+            page,
+            total: Math.ceil(total / itemsPerPage),
+            posts,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: "failure",
+            message: error.message
+        });
+    }
+}
+
+
 module.exports = {
     createPost,
     deletePost,
+    getUserPosts,
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const { createPost, deletePost } = require("../controllers/post");
+const { createPost, deletePost, getUserPosts } = require("../controllers/post");
 const { validateToken } = require('../middlewares/auth');
 
 router.post("/create", validateToken, createPost);
 
 router.delete("/delete/:id", validateToken, deletePost);
+
+router.get("/user/:id/:page?", validateToken, getUserPosts);
 
 
 module.exports = router;
