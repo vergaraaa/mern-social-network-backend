083103635045e97539a0e92276166cb279ed3136
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-18 17:46:16 +0100

    feat: fix upload image bug

--- src/routes/user.js
+++ src/routes/user.js
@@ -29,7 +29,7 @@ router.route("/:id")
     .get(validateToken, getUser)
     .put(validateToken, updateUser);
 
-router.post("/upload-image", uploads.single("file"), uploadImage);
+router.post("/upload-image", [validateToken, uploads.single("file")], uploadImage);
 
 router.get("/image/:file", getImage);
 
